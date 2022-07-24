d20fcc4d4b6c510a0f542f7d40f91fa1f269c3c3
ehmicky <ehmicky@gmail.com>
2022-07-24 21:26:40 +0200

    Add more tests

--- test/prefix.js
+++ test/prefix.js
@@ -3,7 +3,29 @@ import isJsonValue from 'is-json-value'
 import { each } from 'test-each'
 
 each(
-  [{ input: { [Symbol('test')]: undefined }, prefix: '[Symbol(test)]' }],
+  [
+    { input: { [Symbol('test')]: undefined }, prefix: '[Symbol(test)]' },
+    { input: [undefined], prefix: '[0]' },
+    { input: { '': undefined }, prefix: '[""]' },
+    { input: { '.': undefined }, prefix: '["."]' },
+    { input: { 0: undefined }, prefix: '["0"]' },
+    { input: { '0one': undefined }, prefix: '["0one"]' },
+    { input: { '\0': undefined }, prefix: '["\\u0000"]' },
+    { input: { 'one\0': undefined }, prefix: '["one\\u0000"]' },
+    { input: { one: undefined }, prefix: 'one' },
+    { input: { one: { two: undefined } }, prefix: 'one.two' },
+    { input: { one: [undefined] }, prefix: 'one[0]' },
+    { input: [{ one: undefined }], prefix: '[0].one' },
+    { input: { one0: undefined }, prefix: 'one0' },
+    { input: { one_: undefined }, prefix: 'one_' },
+    { input: { _one: undefined }, prefix: '_one' },
+    { input: { one$: undefined }, prefix: 'one$' },
+    { input: { $one: undefined }, prefix: '$one' },
+    // eslint-disable-next-line id-length
+    { input: { a: undefined }, prefix: 'a' },
+    { input: { 'one\u200C': undefined }, prefix: '["one\u200C"]' },
+    { input: { 'one\u00E1': undefined }, prefix: 'one\u00E1' },
+  ],
   ({ title }, { input, prefix }) => {
     test(`Serialize path | ${title}`, (t) => {
       const [{ message }] = isJsonValue(input)
@@ -11,3 +33,8 @@ each(
     })
   },
 )
+
+test('Serialize top-level path', (t) => {
+  const [{ message }] = isJsonValue()
+  t.is(message, 'must not be undefined.')
+})
