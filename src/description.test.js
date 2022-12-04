5e0ba33573ee85f8d8d7f75e1c0ae5343faf493a
ehmicky <ehmicky@gmail.com>
2022-12-04 23:18:09 +0100

    Refactoring

--- src/description.test.js
+++ src/description.test.js
@@ -1,5 +1,6 @@
 import test from 'ava'
 import isJsonValue from 'is-json-value'
+import safeJsonValue from 'safe-json-value'
 import { each } from 'test-each'
 
 // eslint-disable-next-line no-restricted-imports
@@ -80,3 +81,55 @@ test.serial('Returns error name and message', (t) => {
   // eslint-disable-next-line fp/no-delete
   delete Error.prepareStackTrace
 })
+
+each(
+  [
+    {
+      // eslint-disable-next-line fp/no-mutating-methods
+      input: Object.defineProperty({}, 'prop', {
+        value: true,
+        enumerable: true,
+        writable: false,
+        configurable: true,
+      }),
+      reason: 'descriptorNotWritable',
+    },
+    {
+      // eslint-disable-next-line fp/no-mutating-methods
+      input: Object.defineProperty({}, 'prop', {
+        value: true,
+        enumerable: true,
+        writable: true,
+        configurable: false,
+      }),
+      reason: 'descriptorNotConfigurable',
+    },
+    {
+      input: {
+        // eslint-disable-next-line fp/no-get-set
+        get prop() {
+          return true
+        },
+      },
+      reason: 'unresolvedGetter',
+    },
+    {
+      input: {
+        toJSON() {
+          return {}
+        },
+      },
+      reason: 'unresolvedToJSON',
+    },
+  ],
+  ({ title }, { input, reason: expectedReason }) => {
+    test(`Ignores some reasons | ${title}`, (t) => {
+      t.is(isJsonValue(input).length, 0)
+      t.true(
+        safeJsonValue(input).changes.some(
+          ({ reason }) => reason === expectedReason,
+        ),
+      )
+    })
+  },
+)
